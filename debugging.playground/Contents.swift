cfe4558bbd7249504120c68815b52dd8885d2edb
Helena Huang <helena.huang@berkeley.edu>
2015-09-15 00:14:58 -0700

    finished HW1

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,41 +15,43 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]!
+        wordB = words[1]!
     }
-    
-//: [EXPLAIN YOUR ANSWER TO Q1 HERE]
-    
 
-    
+//: ? declares something to be an optional. words is an array of String optionals, but we declared
+//: wordA and wordB to be String types, not String optionals. Therefore, we must unwrap the optionals
+//: using !s.
+
+
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
-        let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+    static func arePalindromes(words: [String]) -> Bool! {
+     let reversedWords = words.map() {String($0.characters.reverse())}
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i : Int = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+//: The integer i was declared with "let," which means the variable should be immutable, instead of "var".
+//: Since the return type is Bool!, the function cannot return nil and should return true instead.
     
-    
-    
+
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
-        var lenA = wordA.characters.count
-        var lenB = wordB.characters.count
+    
+    static func isAnagram(wordA: String, wordB: String) -> Bool {
+        var countLetters = [Character : Int]()
+        let lenA = wordA.characters.count
+        let lenB = wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,21 +77,23 @@ class Foo {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
+//: There was no true return. Since there is no such thing as a nil-Boolean, I changed the return
+//: to Bool, and I switched the nil return to true. The dictionary was not initialized correctly.
 
 
 //: **Do not** change anything below.
 //: You should be able to call the methods as is.
+
 Foo.isAnagram("anagram", wordB: "managra")
 Foo.isAnagram("hello", wordB: "what")
 
